a5436af46514335a9950427a49b9615a64caf21a
Matthias Pawlik <52122163+MrHighsenberg@users.noreply.github.com>
2025-07-13 09:00:41 +0200

    updated XTRG algorithm

--- source/XTRG.jl
+++ source/XTRG.jl
@@ -1,24 +1,25 @@
 module XTRG
-export XTRG_algorithm
+export XTRG_update, XTRG_algorithm
 
 """
-    XTRG_update(rho::Vector, beta::Float64, mode::Bool, Nsweeps::Int, tolerance:Float64)
+    XTRG_update(rho::Vector{<:AbstractArray{<:Number, 4}}, beta::Float64, mode::Bool, Nsweeps::Int, tolerance:Float64)
 
 Function performing a single update of the XTRG algorithm from inverse temperatures beta ---> 2*beta
 
-Input:
-- `rho::Vector`: List of the (canonicalized) local tensors of the MPO corresponding to the quantum state at inverse temperature beta.
+Parameters:
+- `rho::Vector{<:AbstractArray{<:Number, 4}}`: List of the (canonicalized) local tensors of the MPO corresponding to the quantum state at inverse temperature beta.
 - `beta::Float64`: Current inverse temperature of the input state rho.
 - `square::Bool`: If boolean is true initialize the updated rho as the square of rho
 - `Nsweeps::Int`: Number of sweeps performed in the variational DMRG-type optimization along one direction of the chain.
-- `tolerance:Float64`: Threshold value for which the locally optimized tensor rho_new is assumed converged.
+- `tolerance::Float64`: Threshold value for which the locally optimized tensor rho_new is assumed converged.
 
-Output:
-- `rho_new::Vector`: List of (canonicalized) local tensors of the MPO corresponding to the quantum state at inverse temperature 2*beta.
+Returns:
+- `rho_new::Vector{<:AbstractArray{<:Number, 4}}`: List of (canonicalized) local tensors of the MPO corresponding to the quantum state at inverse temperature 2*beta.
 - `beta::Float64`: Increased inverse temperature 2*beta for the state rho_new. 
 """
 function XTRG_update(rho::Vector, beta::Float64, square::Bool, Nsweeps::Int=5, tolerance::Float64=1e-10)
 
+    # Double the inverse temperature
     beta += beta
 
     # Choose initialization mode
@@ -29,11 +30,9 @@ function XTRG_update(rho::Vector, beta::Float64, square::Bool, Nsweeps::Int=5, t
     end
 
     rho2 = deepcopy(rho_init)
-    # Here comes the variational DMRG-type sweeping using the above methods modifying rho_new in place
-    # 
-    # 
-    # 
-    # 
+    
+    # Variational DMRG-type sweeping 
+    
 
     # Evaluate whether the optimization has sufficiently converged
     rho2_diff = add_mpo(rho2, [-rho2_new[1], rho2_new[2:end]])
